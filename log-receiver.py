7f4f5a24fa52b50918b8b4cd61c8b3b457266580
Antti Järvinen <antti.jarvinen@protacon.com>
2019-11-06 21:04:05 +0200

    console print formatting and coloring

--- log-receiver.py
+++ log-receiver.py
@@ -2,15 +2,43 @@
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+class Colors:
+    WHITE = '\u001b[37m'
+    BRIGHT_YELLOW = '\u001b[33;1m'
+    RED = '\u001b[31m'
+    BRIGHT_RED = '\u001b[31;1m'
+    RESET = '\u001b[0m'
+
 class Handler(BaseHTTPRequestHandler):
+    def get_color(self, level):
+        levelColors = {
+            "DEBUG": Colors.WHITE,
+            "WARN": Colors.BRIGHT_YELLOW,
+            "ERROR": Colors.BRIGHT_RED,
+            "FATAL": Colors.RED
+        }
+        return levelColors.get(level, "")
+
     def do_POST(self):
+        receiveIp = self.address_string()
+
         content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        print(post_data.decode('utf-8'))
+        postData = self.rfile.read(content_length).decode('utf-8')
+        logVars = postData.split('|')
+
+        logTime = logVars[0]
+        logName = logVars[1]
+        logLevel = logVars[2].upper()
+        logMessage = logVars[3]
+
+        colorCode = self.get_color(logLevel)
+        
+        print(f"{logTime} - {receiveIp} ({logName}): {colorCode}{logLevel} {logMessage} {Colors.RESET}\n")
         
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
+
+    def log_message(self, format, *args):
+        return
 
 if __name__ == "__main__":
     server_address = ("localhost", 1337)
